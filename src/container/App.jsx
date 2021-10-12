97f0b305d5ce21a4efc0e20adb8ad76a996eed29
Roarco <sebuscaroberth@gmail.com>
2021-10-12 14:18:35 -0500

    Estableciendo la configuracion base para nuestro proyecto, inicializando repositorio local y enlazando con uno remoto, inicializando npm, asi como construyendo la gerarquia de carpetas

---
+++ src/container/App.jsx
@@ -0,0 +1,18 @@
+
+/* este archivo es una mejor forma de trabajar nuestros componetentes,
+ya que aqui los estamos conteniendo en un contenedor
+*/
+/*
+aqui vamos a importar todos los componetentes,
+para  asi tener un contenedor principal con cada,
+uno de ellos
+*/
+import React from 'react'
+
+const App = () => {
+    return(
+        <h1>Ya estamos aqui</h1>
+    )
+}
+
+export default App;
