e82c8055e24d546831e2bdb711558385a27691e4
Roarco <sebuscaroberth@gmail.com>
2021-10-15 23:42:00 -0500

    usando useEffect para consumir la api de la escuela, asi como axios para facilitar las peticiones, e instalando un plugin para babel para que no haya ningun problema, todo esto se hizo en el componente (ProductList)

--- src/container/ProductList.jsx
+++ src/container/ProductList.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+  //useState
+  const [products, setProducts] = useState([]);
+
+  //useEffect
+  useEffect(async () => {
+    const response = await axios(API);
+    setProducts(response.data);
+  }, []);
   return (
     <section className='main-container'>
       <div className='ProductList'>
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem />
+        ))}
       </div>
     </section>
 
